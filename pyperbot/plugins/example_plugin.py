6a81cb87a64316afe6bca94141b83b4d6b047eb8
ellxc <ec486@kent.ac.uk>
2017-12-03 16:33:02 +0000

    Update example_plugin.py

--- pyperbot/plugins/example_plugin.py
+++ pyperbot/plugins/example_plugin.py
@@ -1,22 +1,66 @@
 import datetime
 
-from pyperbot.wrappers import plugin, cron, unload, onload
+from pyperbot.piping import PipeClosed
+from pyperbot.wrappers import plugin, cron, unload, onload, sync, \
+    event, trigger, command, complexcommand, pipeinable_command, regex
 
 
 @plugin
 class example:
+    def __init__(self, bot, config):
+        self.bot = bot
+        self.config = config
+        self.count = 0
+        if 'count' in config:
+            self.count = config['count']
+
     @onload
     def load(self):
-        print("plugin loaded!!!1!!one!!")
+        print("plugin loaded!")
 
     @unload
     def uload(self):
-        print("plugin unloaded!!1one11!!")
+        print("plugin unloaded!")
 
-    @cron("*/2 * * * *")
-    def fizz(self):
-        print("fizz", datetime.datetime.now())
-
-    @cron("*/3 * * * *")
+    @cron("*/5 * * * *")
     def buzz(self):
         print("buzz", datetime.datetime.now())
+        self.count += 1
+
+    @sync
+    def sync(self):
+        print("plugin synced")
+
+    @event('NOTICE')
+    def noticenoticer(self, msg):
+        print("I saw a notice!")
+        self.count += 1
+
+    @trigger(lambda msg: msg.text == 'o.o')
+    def face(self, msg):
+        self.bot.send(msg.reply('O.O'))
+        self.count += 1
+
+    @regex(r'(?P<a>[A-Z])\.(?P=a)')
+    def face2(self, msg, match):
+        self.bot.send(msg.reply("<{a}.{a}>".format(**match.groupdict()).lower()))
+
+    @command('count')
+    def countcommand(self, msg):
+        return msg.reply(self.count)
+
+    @pipeinable_command('face')
+    def trailingface(self, initial, msg):
+        return msg.reply(msg.text + " 0.0")
+
+    @complexcommand('faces')
+    async def beforeandafterface(self, initial, inpipe, outpipe):
+        outpipe.send(initial.reply("o.o"))
+        try:
+            while 1:
+                outpipe.send(await inpipe.recv())
+        except PipeClosed:
+            outpipe.send(initial.reply("o.o"))
+        finally:
+            inpipe.close()
+            outpipe.close()
