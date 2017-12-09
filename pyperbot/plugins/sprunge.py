a3b914c8f1ca49127682ee67afe61bf9b9327f32
Elliot <ec486@kent.ac.uk>
2017-12-09 13:20:42 +0000

    sprunge plugin now contains spam filter reworked to use aiohttp

--- pyperbot/plugins/sprunge.py
+++ pyperbot/plugins/sprunge.py
@@ -1,29 +1,49 @@
-import urllib.parse
-import urllib.request
-
+import async_timeout
+import aiohttp
 from pyperbot.piping import PipeClosed
-from pyperbot.util import schedthreadedfunc
-from pyperbot.wrappers import plugin, complexcommand
+from pyperbot.wrappers import plugin, complexcommand, outputfilter
 
 
 @plugin
 class Sprunge:
+
     @complexcommand
-    async def sprunge(self, initial, inpipe, outpipe):
+    async def sprunge(self, initial, inpipe,  outpipe):
+        dat = []
+        try:
+            while 1:
+                x = await inpipe.recv()
+                dat.append(x)
+        except PipeClosed:
+            async with async_timeout.timeout(10):
+                async with aiohttp.ClientSession() as session:
+                    data = {'f:1': '\n'.join(m.text for m in dat)}
+                    async with session.post('http://ix.io', data=data) as resp:
+                        response = await resp.text()
+            outpipe.send(initial.reply(text=response))
+        finally:
+            outpipe.close()
+            inpipe.close()
+
+    @outputfilter
+    async def spam(self, initial, inpipe, outpipe):
         spam = []
         try:
             while 1:
                 x = await inpipe.recv()
                 spam.append(x)
+                if len(spam) < 4:
+                    outpipe.send(x)
         except PipeClosed:
-                data = {'f:1': '\n'.join(m.text for m in spam)}
-                response = await schedthreadedfunc(urllib.request.urlopen,
-                                                   urllib.request.Request('http://ix.io',
-                                                                          urllib.parse.urlencode(data).encode('utf-8')
-                                                                          )
-                                                   )
-                response = response.read().decode()
-                outpipe.send(initial.reply(text=response))
+            if len(spam) > 4:
+                async with async_timeout.timeout(10):
+                    async with aiohttp.ClientSession() as session:
+                        data = {'f:1': '\n'.join(m.text for m in spam)}
+                        async with session.post('http://ix.io', data=data) as resp:
+                            response = await resp.text()
+                outpipe.send(initial.reply(text="spam detected! here is the output: %s" % response))
+            elif len(spam) == 4:
+                outpipe.send(spam[-1])
         finally:
             outpipe.close()
             inpipe.close()
